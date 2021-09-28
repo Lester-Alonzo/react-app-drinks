3977aa55c6fd5d9194164d915522dd6c3e934ec0
Lester-Alonzo <lestergeo96@gmail.com>
2021-09-28 10:54:07 -0600

    agregando material ui y el modal

--- src/components/Receta.jsx
+++ src/components/Receta.jsx
@@ -1,4 +1,29 @@
+import { useContext, useState } from "react";
+import { ModalContext } from "../context/ModalContext";
+import Box from "@mui/material/Box";
+// import Button from '@mui/material/Button';
+import Typography from "@mui/material/Typography";
+import Modal from "@mui/material/Modal";
+
+const style = {
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: 400,
+  bgcolor: "background.paper",
+  border: "2px solid #000",
+  boxShadow: 24,
+  p: 4,
+};
 const Receta = ({ receta }) => {
+  //configuracion del modal de material ui
+  const [open, setOpen] = useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
+  const { guardarIdReceta } = useContext(ModalContext);
+
   return (
     <div className="col-md-4 mb-3">
       <div className="card">
@@ -9,9 +34,31 @@ const Receta = ({ receta }) => {
           className="card-img-top"
         />
         <div className="card-body">
-          <button type="button" className="btn btn-block btn-primary">
+          <button
+            type="button"
+            className="btn btn-block btn-primary"
+            onClick={() => {
+              guardarIdReceta(receta.idDrink);
+              handleOpen();
+            }}
+          >
             Ver Receta
           </button>
+          <Modal
+            open={open}
+            onClose={handleClose}
+            aria-labelledby="modal-modal-title"
+            aria-describedby="modal-modal-description"
+          >
+            <Box sx={style}>
+              <Typography id="modal-modal-title" variant="h6" component="h2">
+                Desde Modal
+              </Typography>
+              <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+                Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
+              </Typography>
+            </Box>
+          </Modal>
         </div>
       </div>
     </div>
--- src/context/ModalContext.jsx
+++ src/context/ModalContext.jsx
@@ -8,6 +8,22 @@ export const ModalContext = createContext();
 const ModalProvider = (props) => {
   //state del provider
   const [idreceta, guardarIdReceta] = useState(null);
+  const [receta, guardarReceta] = useState({});
+  //una ves que tenemos una receta llamar a la apio
+
+  useEffect(() => {
+    try {
+      const obtenerReceta = async () => {
+        if (!idreceta) return;
+        const url = `https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${idreceta}`;
+        const resultado = await axios(url);
+        guardarReceta(resultado.data.drinks[0]);
+      };
+      obtenerReceta();
+    } catch (err) {
+      console.error(err);
+    }
+  }, [idreceta]);
   return (
     <ModalContext.Provider
       value={{
